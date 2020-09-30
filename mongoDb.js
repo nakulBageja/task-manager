9cb1281f1997cf3528c9da2d0dc531efde101556
unknown <nbageja@gmail.com>
2020-09-30 19:22:15 +0530

    Learning implementation of CRUD operation in mongoDB

--- mongoDb.js
+++ mongoDb.js
@@ -1,7 +1,10 @@
 //CRUD - create read update delete
 
-const MongoDb = require("mongodb"); //npm module
-const MongoClient = MongoDb.MongoClient;
+// const MongoDb = require("mongodb"); //npm module
+// const MongoClient = MongoDb.MongoClient;
+// const ObjectId = MongoDb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb"); //ES6 method || destructuring
 
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -19,9 +22,66 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
     //Inserting first doc into the database
-    db.collection("users").insertOne({
-      name: "Nakul",
-      age: 21
-    });
+
+    //Inserting one data at a time
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Nakul",
+    //     age: 21
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Error during insertion");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //Inserting more than one
+    // db.collection("task").insertMany(
+    //   [
+    //     {
+    //       task: "Do laundary",
+    //       Status: "Incomplete"
+    //     },
+    //     {
+    //       task: "Watch Udemy Course",
+    //       Status: "Completed"
+    //     },
+    //     {
+    //       task: "Take a bath",
+    //       Status: "Incomplete"
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Error during insertion");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //Finding a document in collection using findOne or findMany
+    // db.collection("users").findOne(
+    //   {
+    //     //if you want to search according to ID
+    //     //Then this is the method to since ID is not string but binary
+    //     _id: new ObjectID("5f74788dadf4c23c84314de7")
+    //   },
+    //   (error, user) => {
+    //     if (error) {
+    //       return console.log("ERROR");
+    //     }
+    //     console.log(user);
+    //   }
+    // );
+
+    //Finding all the tasks whose status is incomplete from tasks collection
+    //Using find function
+    db.collection("task")
+      .find({ Status: "Incomplete" })
+      .count((error, count) => {
+        console.log(count);
+      });
   }
 );
